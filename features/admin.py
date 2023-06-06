1555884071ca7bf6436726c015f844ce80203df5
Roshan Dahal <58673204+the-roshandahal@users.noreply.github.com>
2023-06-06 14:54:37 +0545

    admin.py updated

--- features/admin.py
+++ features/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import *
+from django_summernote.admin import SummernoteModelAdmin
 
+
+admin.site.site_header = "Spell Market Admin panel"
 # Register your models here.
+
+# admin.site.unregister(Users)
+class PrivacyFields(SummernoteModelAdmin):
+    summernote_fields = ("privacy_policy","terms_and_condition")
+
+admin.site.register(Cart)
+admin.site.register(Downloads)
+admin.site.register(Token)
+admin.site.register(Contact)
+admin.site.register(PurchasedTemplate)
+admin.site.register(PurchaseSummary)
+admin.site.register(Comments)
+admin.site.register(PrivacyAndTerms,PrivacyFields)
+
+
+admin.site.register(SitemapEntry)
+admin.site.register(Faq)
+
+
+from django.contrib import admin
+
+class DataSetAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        # Prevent deletion of the existing data
+        return False
+
+    def save_model(self, request, obj, form, change):
+        # Limit the creation of only one data set
+        if not obj.pk and Snippet.objects.exists():
+            # Data set already exists, don't save the new one
+            return
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Snippet, DataSetAdmin)
--- themes/admin.py
+++ themes/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import *
+from django_summernote.admin import SummernoteModelAdmin
+
+admin.site.site_header = "Admin panel"
+from django_summernote.models import Attachment
+
+admin.site.unregister(Attachment)
 
 # Register your models here.
+class Description(SummernoteModelAdmin):
+    summernote_fields = ("template_details", "template_features", "template_layout")
+
+
+class BlogField(SummernoteModelAdmin):
+    summernote_fields = "blog"
+
+
+class BlogFields(Description, admin.ModelAdmin):
+    list_display = (
+        "template_name",
+        "released_date",
+    )
+class CatFields(admin.ModelAdmin):
+    list_display = (
+        "category",
+        "order",
+        "status",
+    )
+class SubCatFields(admin.ModelAdmin):
+    list_display = (
+        "sub_category",
+        "category",
+        "order",
+        "status",
+    )
+
+class ChildCatFields(admin.ModelAdmin):
+    list_display = (
+        "child_category",
+        "sub_category",
+        "order",
+        "status",
+    )
+
+admin.site.register(Category,CatFields)
+admin.site.register(SubCategory,SubCatFields)
+admin.site.register(ChildCategory,ChildCatFields)
+admin.site.register(Template, Description)
+admin.site.register(PromoCode)
+# admin.site.register(CompanySetup)
+admin.site.register(Blog, BlogField)
+admin.site.register(Partner)
+admin.site.register(Testimonial)
